c5d7b076b75d76d15f5f9527d2b9e30b509e20fa
leoehler <lennart.oehler@googlemail.com>
2020-06-01 14:06:03 +0200

    Search fragment done without actual search

--- app/src/main/java/de/lingen/hsosna/texview/SearchFragment.java
+++ app/src/main/java/de/lingen/hsosna/texview/SearchFragment.java
@@ -1,6 +1,8 @@
 package de.lingen.hsosna.texview;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +16,12 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
 import java.util.ArrayList;
+
+import de.lingen.hsosna.texview.database.TableArtikelkombination;
+import de.lingen.hsosna.texview.database.TableLagerbestand;
 
 public class SearchFragment extends Fragment {
     private SearchFragmentListener listener;
@@ -22,9 +29,15 @@ public class SearchFragment extends Fragment {
     private EditText editText2;
     private Button button;
 
+
+    private GroceryDBHelper dbHelper;
+    private SQLiteDatabase mDatabase;
+
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private BottomSheetBehavior mBottomSheetBehaviour;
+
 
     /**
      * Interface um Daten an die MainActivity zu senden.
@@ -43,22 +56,36 @@ public class SearchFragment extends Fragment {
         editText = v.findViewById(R.id.editText);
         editText2 = v.findViewById(R.id.editText2);
         button = v.findViewById(R.id.submitButton);
+
+        Context context = getActivity();
+        dbHelper = new GroceryDBHelper(context);
+        mDatabase = dbHelper.getReadableDatabase();
+
+
+        mRecyclerView = v.findViewById(R.id.recyclerVgvhgview_fach01);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        View bottomSheet = v.findViewById(R.id.slideUpjmvgmjhPaneFach01);
+
+        mBottomSheetBehaviour = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehaviour.setState(BottomSheetBehavior.STATE_HIDDEN);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View v) {
                 CharSequence input = editText.getText();
                 CharSequence input2 = editText2.getText();
                 listener.onSearchInputSent(input);
-                mRecyclerView = v.findViewById(R.id.recyclerVgvhgview_fach01);
-                mRecyclerView.setHasFixedSize(true);
-                mLayoutManager = new LinearLayoutManager(getActivity());
 
-                ArrayList<Artikel> ar = new ArrayList<>();
-                ar.add(new Artikel(79987, "Sheldon", 100935, "Punkte, 2,5cm, erika, rose", 140,"FW", "44", "m"));
 
-                mAdapter = new ExampleAdapter(ar);// LIST WITH CONTENTS
-                mRecyclerView.setLayoutManager(mLayoutManager);
+
+                mAdapter = new ExampleAdapter(getListWithContents());// LIST WITH CONTENTS
                 mRecyclerView.setAdapter(mAdapter);
+
+                mBottomSheetBehaviour.setState(BottomSheetBehavior.STATE_EXPANDED);
+
                 //listener.onSearchInputSent(input2);
             }
         });
@@ -82,5 +109,63 @@ public class SearchFragment extends Fragment {
     public void onDetach () {
         super.onDetach();
         listener = null;
+    }
+
+
+
+
+    public ArrayList<Artikel> getListWithContents () {
+        ArrayList<Artikel> artikelListe = new ArrayList<Artikel>();
+//        int lagerort = Integer.parseInt(regalID.subSequence(0, 2).toString());
+//        int regal_nr = Integer.parseInt(regalID.subSequence(2, 4).toString());
+//        int zeile = Integer.parseInt(regalID.subSequence(4, 6).toString());
+        int lagerplatz = 1017;
+        Cursor cursor = mDatabase.rawQuery(
+                "SELECT " + TableLagerbestand.LagerbestandEntry.COLUMN_ARTIKEL_ID + ", "
+                + TableLagerbestand.LagerbestandEntry.COLUMN_GROESSE_ID + ", "
+                + TableLagerbestand.LagerbestandEntry.COLUMN_FARBE_ID + ", "
+                + TableLagerbestand.LagerbestandEntry.COLUMN_FERTIGUNGSZUSTAND + ", "
+                + TableLagerbestand.LagerbestandEntry.COLUMN_MENGE + ", "
+                + TableLagerbestand.LagerbestandEntry.COLUMN_MENGENEINHEIT + ", "
+                + TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_ARTIKEL_BEZEICHNUNG
+                + ", "
+                + TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_FARBE_BEZEICHNUNGEN + ""
+                + " FROM " + TableLagerbestand.LagerbestandEntry.TABLE_NAME + ""
+                + " LEFT JOIN " + TableArtikelkombination.ArtikelkombinationenEntry.TABLE_NAME + ""
+                + " ON " + TableLagerbestand.LagerbestandEntry.COLUMN_ARTIKEL_ID + " = "
+                + TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_ARTIKEL_ID
+                + " AND " + TableLagerbestand.LagerbestandEntry.COLUMN_GROESSE_ID + " = "
+                + TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_GROESSE_ID
+                + " AND " + TableLagerbestand.LagerbestandEntry.COLUMN_FARBE_ID + " = "
+                + TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_FARBE_ID
+                + " WHERE " + TableLagerbestand.LagerbestandEntry.COLUMN_LAGERPLATZ + "="
+                + lagerplatz
+                , null);
+        try {
+            while (cursor.moveToNext()) {
+                int artikelId = cursor.getInt(cursor.getColumnIndex(
+                        TableLagerbestand.LagerbestandEntry.COLUMN_ARTIKEL_ID));
+                int farbId = cursor.getInt(
+                        cursor.getColumnIndex(TableLagerbestand.LagerbestandEntry.COLUMN_FARBE_ID));
+                String menge = cursor.getString(
+                        cursor.getColumnIndex(TableLagerbestand.LagerbestandEntry.COLUMN_MENGE));
+                String mengeneinheit = cursor.getString(cursor.getColumnIndex(
+                        TableLagerbestand.LagerbestandEntry.COLUMN_MENGENEINHEIT));
+                String fertigungszustand = cursor.getString(cursor.getColumnIndex(
+                        TableLagerbestand.LagerbestandEntry.COLUMN_FERTIGUNGSZUSTAND));
+                int groessenId = cursor.getInt(cursor.getColumnIndex(
+                        TableLagerbestand.LagerbestandEntry.COLUMN_GROESSE_ID));
+                String artikelBez = cursor.getString(cursor.getColumnIndex(
+                        TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_ARTIKEL_BEZEICHNUNG));
+                String farbBez = cursor.getString(cursor.getColumnIndex(
+                        TableArtikelkombination.ArtikelkombinationenEntry.COLUMN_FARBE_BEZEICHNUNGEN));
+                Artikel artikel = new Artikel(artikelId, artikelBez, farbId, farbBez, groessenId,
+                        fertigungszustand, menge, mengeneinheit);
+                artikelListe.add(artikel);
+            }
+        } finally {
+            cursor.close();
+        }
+        return artikelListe;
     }
 }
