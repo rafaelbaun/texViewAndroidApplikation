81cd524ec004344025463ec8b1ea1fee87aa5b07
rafaelbaun <baun.rafael@gmail.com>
2020-09-28 16:18:08 +0200

    Login hinzugefügt

---
+++ app/src/main/java/de/lingen/hsosna/texview/Login/URL.java
@@ -0,0 +1,16 @@
+package de.lingen.hsosna.texview.Login;
+
+public class URL {
+
+
+
+    private static String URL = "http://192.168.0.3:8888/";
+
+
+    public static String getURL() {
+        return URL;
+    }
+
+}
+
+
